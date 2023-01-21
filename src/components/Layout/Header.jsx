a03ca4567ab15f9717ec5f1bc21db99ce4f41212
Steven Quintana <stiven0721@hotmail.com>
2023-01-20 19:15:49 -0500

    feat: initialize project and create header

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,17 @@
+import meals from "../../assets/meals.jpg";
+import styles from "./Header.module.css";
+import HeaderCartBtn from "./HeaderCartBtn";
+
+const Header = (props) => (
+  <>
+    <header className={styles.header}>
+      <h1>FoodApp</h1>
+      <HeaderCartBtn />
+    </header>
+    <div className={styles["main-image"]}>
+      <img src={meals} alt="A table with delicious food!" />
+    </div>
+  </>
+);
+
+export default Header;
