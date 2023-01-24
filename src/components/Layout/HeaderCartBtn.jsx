ff20e45fb8b62342b8629e4059050083d7ad6f08
Steven Quintana <stiven0721@hotmail.com>
2023-01-23 21:54:10 -0500

    feat: animated cart btn

--- src/components/Layout/HeaderCartBtn.jsx
+++ src/components/Layout/HeaderCartBtn.jsx
@@ -1,18 +1,40 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../context/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartBtn.module.css";
 
 const HeaderCartBtn = (props) => {
   const cartCtx = useContext(CartContext);
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
-  const cartItems = cartCtx.items.reduce(
+  const { items } = cartCtx;
+
+  const cartItems = items.reduce(
     (currNumber, item) => currNumber + item.amount,
     0
   );
 
+  const buttonStyles = `${styles.button} ${
+    btnIsHighlighted ? styles.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
